bc0f66b847c7f1047943c548d48b9fe564063e27
Lian <mmh847736367@mail.com>
2017-03-17 14:30:36 +0800

    add everyone webpack

---
+++ src/main.js
@@ -0,0 +1,10 @@
+
+class Person {
+	constructor(name) {
+		this.name = name;
+	}
+}
+
+var person = new Person('lian');
+
+console.log(person);
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var inProduction = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+	entry: {
+		app: [
+			'./src/main.js',
+			'./src/main.scss'
+		]
+	},
+	output: {
+		path: path.resolve(__dirname, './public'),
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{
+		        test: /\.s[ac]ss$/,
+		        use: ExtractTextPlugin.extract({
+		        	use: ["css-loader", "sass-loader"],
+		            fallback: "style-loader"   
+		        })
+		    },
+
+			{ 
+				test: /\.js$/, 
+				exclude: /node_modules/, 
+				loader: "babel-loader" 
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractTextPlugin("[name].css"),
+		new webpack.LoaderOptionsPlugin({
+			minimize: inProduction
+		})
+	]
+};
+
+if(inProduction) {
+	module.exports.plugins.push(
+		new webpack.optimize.UglifyJsPlugin()
+	);
+}
+
+
+
+
+
+
+
